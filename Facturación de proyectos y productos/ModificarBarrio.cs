230770119c5b54faef81dd9ec6923fdb5f3fe418
Franco Casuscelli <franco.casuscelli5@gmail.com>
2020-09-11 17:48:13 -0300

    modificacion funcionando

--- Facturación de proyectos y productos/ModificarBarrio.cs
+++ Facturación de proyectos y productos/ModificarBarrio.cs
@@ -61,7 +61,6 @@ namespace Facturación_de_proyectos_y_productos
                 if (reader.Read())
                 {
 
-                    string IdBarrio = reader["id_barrio"].ToString();
                     string nombre = reader["nombre"].ToString();
                     
 
@@ -70,14 +69,6 @@ namespace Facturación_de_proyectos_y_productos
                     BoxBarrio.Text = nombre;
 
                 }
-
-
-
-
-
-
-
-
 
             }
             catch (SqlException ex)
@@ -104,7 +95,74 @@ namespace Facturación_de_proyectos_y_productos
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            
+
+
+            if ((BoxBarrio.Text == ""))
+            {
+                MessageBox.Show("Por favor ingrese el nombre de un barrio", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            else
+            {
+
+                SqlConnection conexion = new SqlConnection();
+
+                //Definimos la cadena de conexion a la base de datos.
+                conexion.ConnectionString = "Data Source=(localdb)\\SQLEXPRESS;Initial Catalog=TPI;Integrated Security=True";
+
+                //La sentencia try...catch nos permite "atrapar" excepciones (Errores) y dar al usuario un mensaje más amigable.
+                try
+                {
+                    //String NombreBarrio = BoxBarrio.Text;
+
+                    //Abrimos la conexion a la base de datos.
+                    conexion.Open();
+
+
+                    //Construimos la consulta sql para buscar el usuario en la base de datos.
+                    String consultaSql = "UPDATE dbo.Barrios SET nombre = @Nombre WHERE id_barrio = '"+this.ID+"'";
+
+                    //Creamos un objeto command para luego ejecutar la consulta sobre la base de datos
+                    SqlCommand command = new SqlCommand(consultaSql, conexion);
+
+                    command.Parameters.AddWithValue("@Nombre", BoxBarrio.Text);
+
+
+
+                    // El metodo ExecuteReader retorna un objeto SqlDataReader con la respuesta de la base de datos. 
+                    // Con SqlDataReader los datos se leen fila por fila, cambiando de fila cada vez que se ejecuta el metodo Read()
+                    try
+                    {
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Modificado con exito!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+
+                }
+                catch (SqlException ex)
+                {
+                    //Mostramos un mensaje de error indicando que hubo un error en la base de datos.
+                    MessageBox.Show(string.Concat("Error de base de datos: ", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    //Preguntamos si el estado de la conexion es abierto antes de cerrar la conexion.
+                    if (conexion.State == ConnectionState.Open)
+                    {
+                        //Cerramos la conexion
+                        conexion.Close();
+                    }
+                }
+
+                this.Close();
+
+            }
         }
     }
 }
